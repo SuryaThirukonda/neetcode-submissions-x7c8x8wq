a2bcfb7be98cadedb6a101269b6898aad9af5b8f
SuryaThirukonda <93106511+SuryaThirukonda@users.noreply.github.com>
2026-08-06 19:26:38 -0500

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if i == len(digits)-1:
+                digits[i]+=1
+            if (digits[i]==10):
+                if i==0:
+                    digits.insert(0,1)
+                    digits[1]=0
+                else:
+                    digits[i]=0
+                    digits[i-1]+=1
+        return digits
